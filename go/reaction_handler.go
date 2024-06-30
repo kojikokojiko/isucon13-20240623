f41b0e54a615e5b396bdc1e42cf68fba260445e3
koji.iwase <koji.iwase@prtimes.co.jp>
2024-06-30 17:51:43 +0900

    戻す

--- go/reaction_handler.go
+++ go/reaction_handler.go
@@ -147,11 +147,7 @@ func fillReactionResponse(ctx context.Context, tx *sqlx.Tx, reactionModel Reacti
 		return Reaction{}, err
 	}
 
-	livestreamModel := LivestreamModel{}
-	if err := tx.GetContext(ctx, &livestreamModel, "SELECT * FROM livestreams WHERE id = ?", reactionModel.LivestreamID); err != nil {
-		return Reaction{}, err
-	}
-	livestream, err := fillLivestreamResponse(ctx, tx, livestreamModel)
+	livestream, err := fetchLivestreamDetailsByID(ctx, tx, reactionModel.LivestreamID)
 	if err != nil {
 		return Reaction{}, err
 	}
